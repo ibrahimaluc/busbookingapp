df5dacfbd46a43a019d98c325fa5f6b0155a970c
ibrahimaluc <ibrahimaluc@ogr.iu.edu.tr>
2023-11-12 15:33:46 +0300

    trips listed in list adapter

---
+++ app/src/main/java/com/ibrahimaluc/busbookingapp/ui/adapter/TripListAdapter.kt
@@ -0,0 +1,42 @@
+package com.ibrahimaluc.busbookingapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ibrahimaluc.busbookingapp.data.remote.Trip
+import com.ibrahimaluc.busbookingapp.databinding.ItemListBinding
+
+class TripListAdapter() :
+    RecyclerView.Adapter<TripListAdapter.TripListViewHolder>() {
+
+    class TripListViewHolder(var binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallBack = object : DiffUtil.ItemCallback<Trip>() {
+        override fun areItemsTheSame(oldItem: Trip, newItem: Trip) =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: Trip, newItem: Trip) =
+            oldItem == newItem
+    }
+    private val listDiffer = AsyncListDiffer(this, diffCallBack)
+
+    var tripList: List<Trip>
+        get() = listDiffer.currentList
+        set(value) = listDiffer.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripListViewHolder {
+        return TripListViewHolder(
+            ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TripListViewHolder, position: Int) {
+        holder.binding.data = tripList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return tripList.size
+    }
+}
---
+++ app/src/main/java/com/ibrahimaluc/busbookingapp/ui/screen/trip/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.ibrahimaluc.busbookingapp.ui.screen.trip
+
+import com.ibrahimaluc.busbookingapp.core.base.BaseViewModel
+
+class ListViewModel :BaseViewModel(){
+}
